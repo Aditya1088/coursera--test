6d4499d8528ca55711a51ba1420862591adc9148
Aditya1088 <64532012+Aditya1088@users.noreply.github.com>
2021-03-11 16:53:46 +0530

    miniproject
    a small project

---
+++ miniproject/js/scripts.js
@@ -0,0 +1,8 @@
+ $("#loginBtn").click(function() {
+            $("#loginModal").modal("show")
+        });
+
+//to dismiss the login Modal when close button is clicked
+        $("#loginClose").click(function(){
+            $("#loginModal").modal("hide")
+        });
